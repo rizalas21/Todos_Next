ccd5e24fe7378b90d9689149c3493b8ecdc29738
rizalas21 <sudrajatrizal1@gmail.com>
2026-02-10 20:33:54 +0700

    FIXED BUG DEPLOYMENT

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { id: string } },
+  context: { params: Promise<{ id: string }> },
 ) {
   try {
-    const { id } = context.params;
+    const { id } = await context.params;
 
     if (!id)
       return NextResponse.json(
@@ -40,10 +40,10 @@ export async function GET(
 
 export async function PUT(
   req: NextRequest,
-  context: { params: { id: string } },
+  context: { params: Promise<{ id: string }> },
 ) {
   try {
-    const { id } = context.params;
+    const { id } = await context.params;
     const data = await req.json();
 
     if (!id)
@@ -79,10 +79,10 @@ export async function PUT(
 
 export async function DELETE(
   req: NextRequest,
-  context: { params: { id: string } },
+  context: { params: Promise<{ id: string }> },
 ) {
   try {
-    const { id } = context.params;
+    const { id } = await context.params;
     console.log("ini params nya: ");
 
     if (!id)
